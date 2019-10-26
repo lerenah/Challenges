08d86f5b10a0e4ad424240e9d5526f55ecde44ad
Lerena Holloway <holloway.lerena@gmail.com>
2019-10-26 19:40:20 -0400

    adds loan calc

--- js_practice/app.js
+++ js_practice/app.js
@@ -13,7 +13,32 @@ loadEventListeners();
 function loadEventListeners() {
   // Add Task event
   form.addEventListener('submit', addTask);
-  clearBtn.addEventListener('onclick', removeTask);
+  document.addEventListener('DOMContentLoaded', getTasks);
+  tasklist.addEventListener('click', removeTask);
+  clearBtn.addEventListener('onclick', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
+}
+
+function getTasks() {
+  let tasks;
+  if (localStorage.getIem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('task'));
+  }
+
+  tasks.forEach(task => {
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+
+    li.appendChild(document.createTextNode(task));
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+
+    taskList.appendChild(li);
+  });
 }
 
 function addTask(e) {
@@ -36,13 +61,83 @@ function addTask(e) {
     // Append li to ul
     taskList.appendChild(li);
 
+    // Store in LS
+    storeTaskInLocalStorage(taskInput.value);
+
     taskInput.value = '';
   }
 
   e.preventDefault();
 }
 
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem('task') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 function removeTask(e) {
-  console.log(e, ' is target');
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    console.log(e.target, ' is target');
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+    }
+  }
+
   e.preventDefault();
+}
+
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    tasks.forEach((task, index) => {
+      if (taskItem.textContent === task) {
+        tasks.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+  console.log(taskItem);
+}
+
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // Faster
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelecterAll('.collection-item').forEach(task => {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text)) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+
+  console.log(text);
 }
